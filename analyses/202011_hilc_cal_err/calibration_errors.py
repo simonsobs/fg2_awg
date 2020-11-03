38ce58ab4a581e0df495cace8e1bf3bb7ce8f2d9
Davide Poletti <davide.pole@gmail.com>
2020-11-03 16:02:12 +0100

    Draft

---
+++ analyses/202011_hilc_cal_err/calibration_errors.py
@@ -0,0 +1,105 @@
+import numpy as np
+import fgbuster as fgb
+import healpy as hp
+import v3_calc as v3
+
+ALMS_NERSC = ''
+NOISE_COV_MATRIX_LOCAL = ''
+CMB_SPECTRA_LOCAL = ''
+LMAX = 4000
+BIN_WIDTH = 20
+FIELDS = 'TT EE BB'.split()
+
+def _import_get_alm():
+    import os
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir('../../releases/202006_hilc_on_planck_so/')
+    from hilc import get_alms
+    os.chdir(dir_path)
+    return get_alms
+
+def _create_cached_noise_matrix():
+    get_alms = _import_get_alm()
+    binned_covs = []
+    freqs = []
+    for field in range(3):
+        alms, freq = get_alms(
+            field, ALMS_NERSC,
+            'tsz ksz cib dust synchrotron freefree ame noise'.split(),
+            'so planck'.split(),
+            lmax=LMAX
+        )
+        cov = fgb.separation_recipes._empirical_harmonic_covariance(alms)
+        lbins = np.arange(1, LMAX+BIN_WIDTH, BIN_WIDTH)
+        lbins[-1] = LMAX+1
+        binned_cov = np.empty(cov.shape[:-1] + lbins[:-1].shape)
+        for i, lmin, lmax in enumerate(zip(lbins[:-1], lbins[1:])):
+            # Average the covariances in the bin
+            lmax = min(lmax, cov.shape[-1])
+            dof = 2 * np.arange(lmin, lmax) + 1
+            binned_cov[..., i] = (dof / dof.sum() * cov[..., lmin:lmax]).sum(-1)
+
+        freqs.append(freqs)
+        binned_covs.append(binned_cov)
+
+    np.savez(NOISE_COV_MATRIX_LOCAL,
+        TT=binned_covs[0],
+        EE=binned_covs[1],
+        BB=binned_covs[2],
+        ells=(lbins[:-1] + lbins[1:]) / 2.,
+        freq_TT=freqs[0],
+        freq_EE=freqs[1],
+        freq_BB=freqs[2],
+    )
+
+def _create_cached_cmb_spectrum():
+    get_alms = _import_get_alm()
+    binned_cls = []
+    for field in range(3):
+        alms = get_alms(field, ALMS_NERSC, ['cmb'], ['so'], lmax=LMAX)[0][-1]
+        cl = hp.alm2cl(alms)
+        lbins = np.arange(1, LMAX+BIN_WIDTH, BIN_WIDTH)
+        lbins[-1] = LMAX+1
+        binned_cl = np.empty(cl.shape[:-1] + lbins[:-1].shape)
+        for i, lmin, lmax in enumerate(zip(lbins[:-1], lbins[1:])):
+            # Average the covariances in the bin
+            lmax = min(lmax, cl.shape[-1])
+            dof = 2 * np.arange(lmin, lmax) + 1
+            binned_cl[..., i] = (dof / dof.sum() * cl[..., lmin:lmax]).sum(-1)
+
+        binned_cls.append(binned_cl)
+
+    np.savez(CMB_SPECTRA_LOCAL,
+        TT=binned_cls[0],
+        EE=binned_cls[1],
+        BB=binned_cls[2],
+        ells=(lbins[:-1] + lbins[1:]) / 2.,
+    )
+   
+
+def get_invN(freqs, field):
+    try:
+        assert get_invN.freqs == freqs
+    except (AttributeError, AssertionError):
+        get_invN.freqs = freqs
+        data = np.load(NOISE_COV_MATRIX_LOCAL)
+        tot_freqs = list(data['freqs'].astype(int))
+        freq_idx = np.array([tot_freqs.index(f) for f in freqs])
+        N = data[FIELDS[field]][freq_idx][:, freq_idx]
+        get_invN.invN = fgb.separation_recipes._regularized_inverse(N)
+    return get_invN.invN
+
+
+def get_bias(delta, freqs=v3.Simons_Observatory_V3_LA_bands()[0]):
+    invN = fgb.separation_recipes._regularized_inverse(
+        np.load(NOISE_COV_MATRIX_LOCAL))
+
+    cmb_dot_cmb = invN.sum((-1, -2))
+    cmb_dot_delta = (delta[None] @ invN.sum(-1))[..., 0]
+    delta_dot_delta = (delta[None] @ invN @ delta[..., None])[..., 0, 0]
+
+    cmb_ps = np.load(CMB_SPECTRA_LOCAL)
+    numerator = 1 + cmb_dot_delta / cmb_dot_cmb
+    denominator = 1 + cmb_ps * (delta_dot_delta - cmb_dot_delta**2 / cmb_dot_cmb)
+    return numerator, denominator
+
